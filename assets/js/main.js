c6420062c3d946f22f376bdee7d60113de4c7b7c
Jean Pedro <jeanjesuspedrobook@gmail.com>
2022-04-26 22:25:22 -0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,10 +28,63 @@ linkDropdown2.forEach((item) => {
         const setaDropdown = document.querySelectorAll('.arrow2');
         setaDropdown.forEach((item) => {
             if (item.getAttribute('src') === "./assets/images/icon-arrow-up.svg") {
-                item.setAttribute('src', "./assets/images/icon-arrow-down.svg")
+                item.setAttribute('src', "./assets/images/icon-arrow-down.svg");
+                const menuDrop = document.querySelector('.dropdown-itens-2');
+                menuDrop.classList.remove('ativo')
             } else {
                 item.setAttribute('src', './assets/images/icon-arrow-up.svg')
+                document.querySelector('.dropdown-itens-2').classList.add('ativo');
             }
         });
     });
+});
+
+const dropMobile = document.querySelector('.dropdown1-mobile');
+const dropMobile2 = document.querySelector('.dropdown2-mobile');
+
+dropMobile.addEventListener('click', function(e) {
+    e.preventDefault();
+    const setaDropdown = document.querySelectorAll('.arrow1');
+    setaDropdown.forEach((item) => {
+        if (item.getAttribute('src') == "./assets/images/icon-arrow-up.svg") {
+            item.setAttribute('src', "./assets/images/icon-arrow-down.svg");
+            document.querySelector('.dropdown-itens-mobile').classList.remove('ativo-mobile');
+        } else {
+            item.setAttribute('src', './assets/images/icon-arrow-up.svg');
+            document.querySelector('.dropdown-itens-mobile').classList.add('ativo-mobile');
+        }
+    })
+});
+
+dropMobile2.addEventListener('click', function(e) {
+    e.preventDefault();
+    const setaDropdown = document.querySelectorAll('.arrow2');
+    setaDropdown.forEach((item) => {
+        if (item.getAttribute('src') === "./assets/images/icon-arrow-up.svg") {
+            item.setAttribute('src', "./assets/images/icon-arrow-down.svg");
+            document.querySelector('.dropdown-itens-2-mobile').classList.remove('ativo-mobile');
+        } else {
+            item.setAttribute('src', './assets/images/icon-arrow-up.svg');
+            document.querySelector('.dropdown-itens-2-mobile').classList.add('ativo-mobile');
+        }
+    })
+    
+});
+
+
+const menuHamburguer = document.querySelector('.icon-menu');
+
+menuHamburguer.addEventListener('click', function() {
+    const navBar = document.querySelector('.navbar');
+    const liPrincipal = document.querySelector('.li-principal');
+    const imgHamburguer = document.querySelectorAll('.img-menu');
+    navBar.classList.toggle('ativar');
+    liPrincipal.classList.toggle('ativar');
+    imgHamburguer.forEach((item) => {
+        if(item.getAttribute('src') === "./assets/images/icon-menu.svg") {
+            item.setAttribute('src', "./assets/images/icon-close-menu.svg")
+        } else {
+            item.setAttribute("src", "./assets/images/icon-menu.svg")
+        }
+    })
 });
